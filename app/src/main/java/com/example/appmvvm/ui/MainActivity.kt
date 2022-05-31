208a41e2a69b9b791f897b8696c1e1dd9f8575e2
NikitaRybakov21 <nikitarybakov044@gmail.com>
2022-05-31 23:48:44 +0300

    /----DZ_6----/

--- app/src/main/java/com/example/appmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/example/appmvvm/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.appmvvm.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.appmvvm.R
+import com.example.mylibrary.TestModule
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -10,18 +11,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val splash = instalSplashScreen()
+   /*   val splash = instalSplashScreen()
         splash.setKeepOnScrenCondition  { true }
 
         Executors.newSingleThreadExecutor().execute {
             Thread.sleep(3000)
             splash.setKeepOnScrenCondition { false }
-        }
-
+        }  */
 
         setContentView(R.layout.activity_main)
-
         setFirstFragment()
+
+        TestModule.test("test string")
     }
 
     private fun setFirstFragment() {
